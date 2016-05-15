395d12f335ca4ef0aac84725e3885e5c90666f0c
NotMegalion <megalion@MegaliosMacBook.austin.rr.com>
2016-05-15 16:27:02 -0500

    Alpha build

--- js/popup.js
+++ js/popup.js
@@ -1,40 +1,53 @@
 $(document).ready(function()
 {
   console.log("loaded.");
+  printBookmarks('0');
+
+
   //chrome.bookmarks.getTree(function(e){window.tree = e; $("body").html()})
   $("#add-bookmark").click(function()
   {
     var value = $("#bookmark-input").val(),
         substring = "https://",
         subsubstring = "http://";
-        if(value.indexOf(substring) > -1 && value.indexOf(subsubstring > -1)){
-    //chrome.tabs.create({url: value});
+        if(value.indexOf(substring) > -1 || value.indexOf(subsubstring) > -1){
     chrome.bookmarks.create({title: value, url: value});
+    $("#bookmark-list").empty();
+    printBookmarks('0');
+document.geti("bookmark-input").reset();
   }
   else{
-    //chrome.tabs.create({url: substring+value});
     chrome.bookmarks.create({title: value, url: substring+value});
+    $("#bookmark-list").empty();
+    printBookmarks('0');
+document.geti("bookmark-input").reset();
   }
 
-    //chrome.bookmarks.create({'title': 'Extension bookmarks', 'url': '+value+'});
-    //$("#bookmark-list").append("<li><button id=>"+value+"</button></li>");
   });
-  function process_bookmark(bookmarks) {
+  $("#list-bookmark").click(function(){
+    $("#bookmark-list").empty();
+    printBookmarks('0');
+  });
+  function printBookmarks(id) {
+   chrome.bookmarks.getChildren(id, function(children) {
+      children.forEach(function(bookmark) {
+        console.log(bookmark.title);
+        var id = $("#bookmark-list").children().length;
+        var bookmarkLocal = bookmark;
+        $("#bookmark-list").append("<li id='"+id+"'>"+bookmark.title+"<p id='x"+id+"' style='color:red; display: inline-block; margin:0px; margin-left:5px;'>X</p></li>");
+        $("#"+id).click(function(e){
+          if (e.target !== this)
+            return;
+          chrome.tabs.create({url: bookmarkLocal.url})
+        });
+        $("#x"+id).click(function(){
+          chrome.bookmarks.remove(bookmarkLocal.id);
+          $("#bookmark-list").empty();
+          printBookmarks('0');
+        });
+        printBookmarks(bookmark.id);
+      });
+   });
+  }
 
-    for (var i =0; i < bookmarks.length; i++) {
-        var bookmark = bookmarks[i];
-        if (bookmark.url) {
-            console.log("bookmark: "+ bookmark.title + " ~  " + bookmark.url);
-        }
-
-        if (bookmark.children) {
-            process_bookmark(bookmark.children);
-        }
-    }
-}
-
-chrome.browserAction.onClicked.addListener(function(tab) {
-  console.log("listing bookmarks: " );
-  chrome.bookmarks.getTree( process_bookmark );
-});
 });
